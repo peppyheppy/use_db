4311da14e765ddf8c0ef78cb389c9004c9b11e53
remi <remi@remitaylor.com>
2008-10-10 15:25:08 -0700

    update to work properly if models are loaded inside of ERB, eg. in fixtures

--- lib/use_db.rb
+++ lib/use_db.rb
@@ -55,7 +55,7 @@ module UseDbPlugin
       return options
     else
       str = "#{prefix}#{rails_env}#{suffix}"
-      connections = YAML.load(ERB.new(IO.read("#{RAILS_ROOT}/config/database.yml")).result)
+      connections = YAML.load(ERB.new(IO.read("#{RAILS_ROOT}/config/database.yml"), nil, nil, '_use_db_erbout').result)
       raise "Cannot find database specification.  Configuration '#{str}' expected in config/database.yml" if (connections[str].nil?)      
       return connections[str]
     end
